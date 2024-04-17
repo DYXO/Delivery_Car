bd556393ea2e19603788a00d182261945c27949d
DYXO <149788976+DYXO@users.noreply.github.com>
2024-04-17 18:49:34 +0100

    Update motor.cpp

--- motor_control/src/motor.cpp
+++ motor_control/src/motor.cpp
@@ -1,26 +1,69 @@
 #include <cstdio>
 #include <pigpio.h>
 
-#define leftmotor 17
-#define rightmotor 18
+#define leftmotor1 17
+#define leftmotor2 25
+#define rightmotor1 18
+#define rightmotor2 24
+#define leftsensor 23
+#define rightsensor 22
 class motor
 {
 public:
-        void forward(int speed)
+	void SetMotor(int Lspeed,int Rspeed)
 	{
-		gpioPWM(leftmotor, speed);
-		gpioPWM(rightmotor,speed);		
+		if(Lspeed>255||Rspeed>255||Lspeed<-255||Rspeed<-255)
+		{
+			printf("erro!\n");
+			return;
+		}
+		else
+		{
+			if(Lspeed>=0)
+			{
+				gpioPWM(leftmotor1, Lspeed);
+				gpioPWM(leftmotor2, 0);
+			}
+			else if(Lspeed<0)
+			{
+				Lspeed=-Lspeed;
+				gpioPWM(leftmotor1, 0);
+				gpioPWM(leftmotor2, Lspeed);
+			}
+			if(Rspeed>=0)
+			{
+				gpioPWM(rightmotor1, Rspeed);
+				gpioPWM(rightmotor2, 0);
+			}
+			else if(Rspeed<0)
+			{
+				Rspeed=-Rspeed;
+				gpioPWM(rightmotor1, 0);
+				gpioPWM(rightmotor2, Rspeed);
+			}
+		}
+		return;
 	}
-	void turnleft(int left_speed,int right_speed)
-	{
-		gpioPWM(leftmotor, left_speed);
-		gpioPWM(rightmotor,right_speed);
-	}
-	void turnright(int left_speed,int right_speed)
-	{
-		gpioPWM(leftmotor, left_speed);
-		gpioPWM(rightmotor,right_speed);
-	}
+};
+
+class infrared
+{
+public:
+        infrared()
+        {
+        	gpioSetMode(leftsensor,PI_INPUT);
+        	gpioSetMode(rightsensor,PI_INPUT);
+        	gpioSetPullUpDown(leftsensor, PI_PUD_UP);
+        	gpioSetPullUpDown(rightsensor, PI_PUD_UP);
+        }
+        int readleft()
+        {
+        	return gpioRead(leftsensor);
+        }
+        int readright()
+        {
+        	return gpioRead(rightsensor);        
+        }
 	
 };
 
@@ -28,19 +71,28 @@ int main(int argc, char ** argv)
 {
   	(void) argc;
   	(void) argv;
+  	while(gpioInitialise() < 0);
   	motor DeliveryRobot_motor;
-  	
-	while(gpioInitialise() < 0);
+  	infrared DeliveryRobot_Sensor;
 	while(1)
 	{
-		
-		DeliveryRobot_motor.turnleft(100,255);
-		
-		gpioSleep(PI_TIME_RELATIVE, 1, 0);
-
-		DeliveryRobot_motor.turnright(0,0);
-		
-		gpioSleep(PI_TIME_RELATIVE, 1, 0);
+		if(DeliveryRobot_Sensor.readleft()==0&&DeliveryRobot_Sensor.readright()==0)
+		{
+		DeliveryRobot_motor.SetMotor(-140,-140);
+		}
+		else if(DeliveryRobot_Sensor.readleft()==0&&DeliveryRobot_Sensor.readright()==1)
+		{
+		DeliveryRobot_motor.SetMotor(-100,200);
+		}
+		else if(DeliveryRobot_Sensor.readleft()==1&&DeliveryRobot_Sensor.readright()==0)
+		{
+		DeliveryRobot_motor.SetMotor(200,-100);
+		}		
+		else if(DeliveryRobot_Sensor.readleft()==1&&DeliveryRobot_Sensor.readright()==1)
+		{
+		DeliveryRobot_motor.SetMotor(0,0);
+		}
+		//gpioSleep(PI_TIME_RELATIVE,0,10000);
 	}
 	printf("hello world my_package package\n"); 
 	return 0;	
